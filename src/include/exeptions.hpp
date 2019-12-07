2b413e6d6c295b19bafc11b4ddd4c5c41b7406af
liember <liember@yandex.ru>
2019-12-07 13:40:12 +0700

    add file existing extention

--- src/include/exeptions.hpp
+++ src/include/exeptions.hpp
@@ -13,3 +13,15 @@ public:
         return "[!] Property already created and linked with other propertyes";
     }
 };
+
+class fileNotExist : public std::exception
+{
+private:
+    std::string info;
+
+public:
+    virtual const char *what() const throw()
+    {
+        return "[!] File does not exist or path specified";
+    }
+};
