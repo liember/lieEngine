9e7fd47aa01cbc5b6271df2e878922f21229bc39
liember <liember@yandex.ru>
2019-12-08 14:07:56 +0700

    gravity and planets now works!

--- src/userConstrutorSpace.cpp
+++ src/userConstrutorSpace.cpp
@@ -1,55 +1,57 @@
-#include <iostream>
+#include "includer.hpp"
+#include <math.h>
 
-#include "include/objectsManager.hpp"
-#include "include/game.hpp"
-#include "include/timer.hpp"
-#include "include/gameobj.hpp"
-#include "include/property.hpp"
+//if you need this
+gameobj *obj;
+positionProperty *pos;
+speedProperty *vel;
+textureProperty *tex;
+positionLimitProperty *lim;
 
-#include "include/positionProperty.hpp"
-#include "include/positionLimitProperty.hpp"
-#include "include/speedProperty.hpp"
-#include "include/textureProperty.hpp"
-#include "include/WASDAnimatorProperty.hpp"
-#include "include/WASDControllerProperty.hpp"
-#include "include/tilemapProperty.hpp"
-#include "include/textureProperty.hpp"
-
-#include "include/cameraProperty.hpp"
+//planets
+planet *pl[3];
 
 void game::initGame()
 {
-    gameobj *obj = new gameobj();
-    positionProperty *p = new positionProperty(obj);
-    p->setX(100);
-    p->setY(100);
-    obj->connectProperty(p);
-    obj->addProperty(PROPERTY_SPEED);
-    textureProperty *t = new textureProperty(obj);
-    t->setTexture("assets/b2.png");
-    obj->connectProperty(t);
-    gmsManager.add(obj);
-
-    obj = new gameobj();
-    p = new positionProperty(obj);
-    p->setX(10);
-    p->setY(10);
-    obj->connectProperty(p);
-    obj->addProperty(PROPERTY_SPEED);
-    t = new textureProperty(obj);
-    t->setTexture("assets/b2.png");
-    obj->connectProperty(t);
-    gmsManager.add(obj);
-
     obj = new gameobj();
     obj->addProperty(PROPERTY_TILEMAP);
     gmsManager.add(obj);
+
+    pl[1] = new planet();
+    pl[1]->setPos(10, 200);
+    pl[1]->setVel(202, 112);
+    gmsManager.add(pl[1]);
+
+    pl[2] = new planet();
+    pl[2]->setPos(200, 10);
+    gmsManager.add(pl[2]);
 
     obj = new gameobj();
     obj->addProperty(PROPERTY_CAMERA);
     gmsManager.add(obj);
 }
 
+//world gravitation
 void game::updateGame()
 {
+    double x1 = pl[1]->getPosX();
+    double y1 = pl[1]->getPosY();
+
+    double x2 = pl[2]->getPosX();
+    double y2 = pl[2]->getPosY();
+
+    double hypotenuse = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    if (hypotenuse == 0)
+    {
+        throw new zeroRadius();
+    }
+
+    double gravity = 0.00001;
+
+    double potence = gravity * (pl[1]->getMass() * pl[2]->getMass()) / pow(hypotenuse, 2);
+
+    double xVel = -(x1 - x2) / hypotenuse * potence;
+    double yVel = -(y1 - y2) / hypotenuse * potence;
+    pl[1]->addVel(xVel, yVel);
+    std::cout << xVel << " " << yVel << std::endl;
 }
