80843d13d0f206690664b79643e9d60aef6a3658
liember <liember@yandex.ru>
2019-12-08 14:06:42 +0700

    create includer file

---
+++ src/includer.hpp
@@ -0,0 +1,21 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#include "include/objectsManager.hpp"
+#include "include/game.hpp"
+#include "include/timer.hpp"
+#include "include/gameobj.hpp"
+#include "include/property.hpp"
+
+#include "include/positionProperty.hpp"
+#include "include/positionLimitProperty.hpp"
+#include "include/speedProperty.hpp"
+#include "include/textureProperty.hpp"
+#include "include/WASDAnimatorProperty.hpp"
+#include "include/WASDControllerProperty.hpp"
+#include "include/tilemapProperty.hpp"
+#include "include/textureProperty.hpp"
+
+#include "include/planet.hpp"
+
+#include "include/exeptions.hpp"
