9d8b29dffabe43f311cb622021787370e2aacfca
liember <liember@yandex.ru>
2019-12-08 14:05:40 +0700

    add includer

--- src/game.cpp
+++ src/game.cpp
@@ -1,20 +1,4 @@
-#include <SDL2/SDL.h>
-#include <iostream>
-
-#include "include/objectsManager.hpp"
-#include "include/game.hpp"
-#include "include/timer.hpp"
-#include "include/gameobj.hpp"
-#include "include/property.hpp"
-
-#include "include/positionProperty.hpp"
-#include "include/positionLimitProperty.hpp"
-#include "include/speedProperty.hpp"
-#include "include/textureProperty.hpp"
-#include "include/WASDAnimatorProperty.hpp"
-#include "include/WASDControllerProperty.hpp"
-#include "include/tilemapProperty.hpp"
-#include "include/textureProperty.hpp"
+#include "includer.hpp"
 
 using namespace std;
 
