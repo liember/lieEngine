31cb11a5f7245a1938609b15fcdfdb3c68f1bdf1
liember <liember@yandex.ru>
2019-12-17 06:43:39 +0700

    it works and don't care how!!!

--- src/userConstrutorSpace.cpp
+++ src/userConstrutorSpace.cpp
@@ -18,7 +18,8 @@ buttonProperty *quitB;
 buttonProperty *darwLines;
 bool draw = false;
 
-double gravity = 0.0000000000667408;
+double gravity = 0.0000667408;
+int nearK = 70;
 
 void game::initGame() {
   //############ menu ##############
@@ -59,54 +60,84 @@ void game::initGame() {
   //############ player ##############
 
   playerShip *pla = new playerShip();
-  pla->setPos(900, 450);
+  pla->setPos(0, 149 * nearK + 200);
   pla->setTex("assets/bird.png");
-  pla->setVel(0.3, 3);
+  pla->setVel(0 , -100);
   pla->setMass(1);
   pla->setSize(50);
   pla->setSpeed(0.01);
   gmsManager.add(pla);
-  pl[3] = pla;
+  pl[8] = pla;
 
   //############ sun ##############
   // mass 1,989 e30
   // rad 695510
-  pl[0] = new planet("sun", 100, 19890000, 0, 0, 0, 0, "assets/sun.png");
+  pl[0] = new planet("sun", 695, 198900000, 0, 0, 0, 0, "assets/sun.png");
   gmsManager.add(pl[0]);
 
   //############ planets ##############
+  
   // mnercury
   // mass 3,285 e23
   // rad 2439,7
-  //~~58 000 000
-  pl[1] = new planet("mercury", 48, 3285, 0, -480, 0, 116);
+  //Перигелий	46 001 009 км
+  //speed 48
+  pl[1] = new planet("mercury", 2 * 3, 3285, -48, 0, 0, 46 * nearK);
   gmsManager.add(pl[1]);
 
   // venus
   // mass 4,867 e24
   // rad 6051,7
   //~~108 000 000
-  pl[2] = new planet("venus", 120, 4867, 0, -35, 0, 216);
+  //speed 35
+  pl[2] = new planet("venus", 6 * 3, 4867, -35, 0, 0, 108 * nearK);
   gmsManager.add(pl[2]);
 
   // earth
   // mass 5,972 e24
   // rad 6371
   // 149 600 000
-  pl[3] = new planet("earth", 126, 5972, 0, -30, 0, 300);
+  //speed 30
+  pl[3] = new planet("earth", 6 * 3, 5972, -30, 0, 0, 149 * nearK);
   gmsManager.add(pl[3]);
 
   // mars
   // mass 6,39 e23
   // rad 3 389,5
   // 228 000 000
-  pl[4] = new planet("venus", 66, 639, 0, -24, 0, 456);
+  //speed 24.13
+  pl[4] = new planet("mars", 2 * 3, 639, -24.13, 0, 0, 228 * nearK);
   gmsManager.add(pl[4]);
+
+  // jupiter
+  // mass 1.898 e27
+  // rad 69 911.5
+  // 778,57 000 000
+  //speed 24.13
+  pl[5] = new planet("jupiter", 69 * 3, 18980000, -13, 0, 0, 778 * nearK );
+  gmsManager.add(pl[5]);
+
+  // uran
+  // mass 8,6813⋅10 e25
+  // rad 25 559 
+  // 2,8 000 000 000
+  //speed 6.81
+  pl[6] = new planet("uran", 25 * 3, 8681300, -6.81, 0, 0, 2800 * nearK );
+  gmsManager.add(pl[6]);
+
+  // neptun 
+  // mass 1,0243 e26
+  // rad 24 764
+  // 4 452 940 833
+  //speed 5.439
+  pl[7] = new planet("neptun", 24 * 3, 10243000, -5.439, 0, 0, 4452 * nearK );
+  gmsManager.add(pl[7]);
 
   obj = new gameobj();
   pos = new positionProperty(obj);
   obj->connectProperty(pos);
   cam = new camera(obj);
+  cam->set(0,-149 * nearK - 100);
   obj->connectProperty(cam);
   gmsManager.add(obj);
 }
@@ -121,8 +152,8 @@ void game::updateGame() {
     draw = !draw;
   }
 
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
       if (i != j) {
         double x1 = pl[j]->getPosX();
         double y1 = pl[j]->getPosY();
@@ -148,8 +179,8 @@ void game::updateGame() {
 
 void game::specificRenderBefore() {
   if (draw) {
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 9; i++) {
+      for (int j = i; j < 9; j++) {
         if (i != j) {
           double x1 = pl[j]->getGlobX();
           x1 += pl[j]->getSize() / 2;
