1ebb9a191918b3f1be0d071b1031de9da10caa0a
liember <liember@yandex.ru>
2019-12-20 04:41:58 +0700

    f

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include "include/game.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
   game *gae = nullptr;
   gae = new game();
   gae->initEngine("lie and ember", SDL_WINDOWPOS_CENTERED,
                   SDL_WINDOWPOS_CENTERED, 1200, 600, false);
-  try {
-    while (gae->running()) {
+  try
+  {
+    while (gae->running())
+    {
       gae->handleevents();
       gae->update();
       gae->render();
     }
-  } catch (const std::exception &e) {
+  }
+  catch (const std::exception &e)
+  {
     std::cerr << e.what() << '\n';
   }
 }
