6a6fb7ab731f77d665975fcb4660862969599a32
liember <liember@yandex.ru>
2019-12-20 04:40:56 +0700

    new useful buttons

---
+++ src/include/button.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../includer.hpp"
+
+class button : public gameobj
+{
+private:
+    const bool positionary;
+
+    positionProperty *pos;
+    textProperty *text;
+    clickAreaProperty *area;
+
+public:
+    bool isClecked();
+    bool isHovered();
+
+    void setSize(int x, int y);
+    void setPaddings(int x, int y);
+    void setPos(int x, int y);
+
+    void setBorederColor(int r, int g, int b);
+    void setBgColor(int r, int g, int b);
+    void setClickColor(int r, int g, int b);
+    void setHoverColor(int r, int g, int b);
+    void setTextColor(int r, int g, int b);
+
+    void setDelay(double d);
+
+    void press();
+
+    button(int x, int y, const char *text);
+    ~button();
+};
