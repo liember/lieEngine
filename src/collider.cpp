c53f983558f4637c55eb693461241adfdb314bf5
liember <liember@yandex.ru>
2019-12-20 04:35:40 +0700

    i dont now why git has add this file in changed

--- src/collider.cpp
+++ src/collider.cpp
@@ -4,20 +4,23 @@ std::vector<colliderProperty *> colliderProperty::colliders;
 
 PropertyControlSystem *colliderProperty::getPcs() { return p; }
 
-colliderProperty::colliderProperty(PropertyControlSystem *pcs) {
+colliderProperty::colliderProperty(PropertyControlSystem *pcs)
+{
   setID(PROPERTY_COLLIDER);
   p = pcs;
   pos = static_cast<positionProperty *>(pcs->getProperty(PROPERTY_POSITION));
   colliders.push_back(this);
 }
 
-colliderProperty::~colliderProperty() {
+colliderProperty::~colliderProperty()
+{
   std::vector<colliderProperty *>::const_iterator it =
       find(colliders.begin(), colliders.end(), this);
   colliders.erase(it);
 }
 
-bool colliderProperty::detect(SDL_Rect b) {
+bool colliderProperty::detect(SDL_Rect b)
+{
   int left = std::max(body.x, b.x);
   int top = std::min(body.y + body.h, b.y + b.h);
   int right = std::min(body.x + body.w, b.x + b.w);
@@ -32,10 +35,14 @@ bool colliderProperty::detect(SDL_Rect b) {
   return true;
 }
 
-PropertyControlSystem *colliderProperty::findCollision() {
-  for (colliderProperty *c : colliders) {
-    if (c != this) {
-      if (detect(c->getBody())) {
+PropertyControlSystem *colliderProperty::findCollision()
+{
+  for (colliderProperty *c : colliders)
+  {
+    if (c != this)
+    {
+      if (detect(c->getBody()))
+      {
         return c->getPcs();
       }
     }
@@ -45,25 +52,29 @@ PropertyControlSystem *colliderProperty::findCollision() {
 
 void colliderProperty::setDraw(bool b) { drawBody = b; }
 
-void colliderProperty::draw() {
+void colliderProperty::draw()
+{
   SDL_Rect bufRect = body;
   bufRect.x = pos->getX();
   bufRect.y = pos->getY();
-  if (drawBody) {
+  if (drawBody)
+  {
     SDL_SetRenderDrawColor(game::getRenderer(), 0, 0, 0, 0);
     SDL_RenderDrawRect(game::getRenderer(), &bufRect);
     SDL_SetRenderDrawColor(game::getRenderer(), 255, 255, 255, 0);
   }
 }
 
-void colliderProperty::update() {
+void colliderProperty::update()
+{
   body.x = pos->getOnlyX();
   body.y = pos->getOnlyY();
 }
 
 SDL_Rect colliderProperty::getBody() { return body; }
 
-void colliderProperty::setBody(int w, int h) {
+void colliderProperty::setBody(int w, int h)
+{
   body.w = w;
   body.h = h;
 }
