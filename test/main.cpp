cf743b1db3b1ee06a5a991cc37edb6d36ed37e10
liember <liember@yandex.ru>
2020-02-29 07:31:42 +0700

    delete

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,23 @@
+#include "core.hpp"
 #include <iostream>
 
 int main()
 {
-    return 0;
+    game *gae = nullptr;
+    gae = new game();
+    gae->initEngine("lie and ember", SDL_WINDOWPOS_CENTERED,
+                    SDL_WINDOWPOS_CENTERED, 1200, 600, false);
+    try
+    {
+        while (gae->running())
+        {
+            gae->handleevents();
+            gae->update();
+            gae->render();
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
