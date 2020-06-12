ec38194724a17bf98d60abf74e73781928dcf0cc
liember <liember@yandex.ru>
2020-06-12 23:28:59 +0700

    add pcs based object

---
+++ include/object.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "property.hpp"
+
+#include <map>
+#include <memory>
+#include <vector>
+
+class object;
+
+typedef std::unique_ptr<propertyes::property> prop_ptr;
+typedef std::map<int, prop_ptr> property_collection;
+
+typedef std::shared_ptr<object> obj_ptr;
+typedef std::unique_ptr<object> unique_obj;
+
+class object
+{
+protected:
+  property_collection active_properties;
+  property_collection passive_properties;
+
+  std::vector<unique_obj> childs;
+
+public:
+  void KillChilds();
+  void AddChild(unique_obj obj);
+
+  void Update();
+
+  void AddProperty(prop_ptr p);
+  prop_ptr StealProperty(int id);
+};
---
+++ src/object.cpp
@@ -0,0 +1,55 @@
+#include "property.hpp"
+#include "object.hpp"
+
+void object::Update()
+{
+  for (auto &&i : active_properties)
+  {
+    i.second->Update();
+  }
+}
+
+void object::AddProperty(prop_ptr p)
+{
+  int key = p->GetType();
+
+  if (p->GetUpdateStatus() == propertyes::status::Active)
+    active_properties[key] = std::move(p);
+  else
+    active_properties[key] = std::move(p);
+}
+
+prop_ptr object::StealProperty(int id)
+{
+  prop_ptr res;
+  auto it = active_properties.find(id);
+  if (it != active_properties.end())
+  {
+    res = std::move(it->second);
+    active_properties.erase(it);
+  }
+  it = active_properties.find(id);
+  if (it != passive_properties.end())
+  {
+    res = std::move(it->second);
+    passive_properties.erase(it);
+  }
+  else
+  {
+    //throw Except("Cant find property for stealing");
+  }
+  return res;
+}
+
+void object::KillChilds()
+{
+  for (auto &&i : childs)
+  {
+    delete i.release();
+  }
+}
+
+void object::AddChild(unique_obj obj)
+{
+  childs.push_back(std::move(obj));
+}
