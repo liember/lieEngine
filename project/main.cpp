0499479eec55a9ebea8e2ae361a872f6bed2e105
liember <liember@yandex.ru>
2020-08-21 23:59:54 +0700

    new core

--- project/main.cpp
+++ project/main.cpp
@@ -3,21 +3,13 @@
 
 int main()
 {
-  game *gae = nullptr;
-  gae = new game();
-  gae->initEngine("lie and ember", SDL_WINDOWPOS_CENTERED,
-                  SDL_WINDOWPOS_CENTERED, 1200, 600, false);
-  try
+  Core::Game gae;
+  gae.initEngine("lie and ember", SDL_WINDOWPOS_CENTERED,
+                 SDL_WINDOWPOS_CENTERED, 1200, 600, false);
+  while (gae.running())
   {
-    while (gae->running())
-    {
-      gae->handleevents();
-      gae->update();
-      gae->render();
-    }
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << e.what() << '\n';
+    gae.handleevents();
+    gae.update();
+    gae.render();
   }
 }
--- src/core.cpp
+++ src/core.cpp
@@ -1,109 +1,197 @@
 #include "core.hpp"
 
-using namespace std;
+using namespace Core;
 
-SDL_Renderer *game::renderer = nullptr;
-SDL_Window *game::window = nullptr;
-
-SDL_Event game::event;
-
-game::game(/* args */) {}
-
-game::~game()
+Texture::Texture(std::string file_name, SDL_Renderer *rend)
 {
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  TTF_Quit();
+    renderer = rend;
+    CheckFile(file_name.c_str());
+    SDL_Surface *tempSurf = IMG_Load(file_name.c_str());
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, tempSurf);
+    SDL_FreeSurface(tempSurf);
+};
+
+Texture::~Texture()
+{
+    SDL_DestroyTexture(_texture);
 }
 
-void game::initEngine(const char *title, int xpos, int ypos, int width,
+void Texture::CheckFile(const char *file_name)
+{
+    std::ifstream file;
+    file.open(file_name);
+    if (!file)
+        throw new std::exception();
+}
+
+void Texture::Draw(int x, int y, int width, int height)
+{
+    SDL_Rect dest;
+    dest.x = x;
+    dest.y = y;
+    dest.w = width;
+    dest.h = height;
+    SDL_RenderCopy(renderer, _texture, nullptr, &dest);
+}
+
+Timer::Timer()
+{
+    creationTime = GetCurrentTime();
+}
+
+Timer::~Timer() {}
+
+double Timer::GetCurrentTime()
+{
+    using Duration = std::chrono::duration<double>;
+    return std::chrono::duration_cast<Duration>(
+               std::chrono::high_resolution_clock::now().time_since_epoch())
+        .count();
+}
+
+bool Timer::Timeout(double source, double delay)
+{
+    if (source + delay < GetCurrentTime())
+    {
+        return true;
+    }
+    return false;
+}
+
+ObjectsManager::ObjectsManager() : draw(new std::queue<Object &>())
+{
+}
+
+ObjectsManager::~ObjectsManager()
+{
+    DestroyAll();
+}
+
+void ObjectsManager::Update()
+{
+    for (auto &&i : update_list)
+    {
+        if (cur_update % i->update_prior == 0)
+        {
+            i->Update();
+        }
+
+        if (i->draw)
+        {
+            draw->push(*i);
+        }
+    }
+
+    if (cur_update < max_update_prior)
+    {
+        cur_update++;
+    }
+    else
+    {
+        cur_update = 0;
+    }
+}
+
+void ObjectsManager::Draw()
+{
+    while (!draw->empty())
+    {
+        Object &obj = draw->front();
+        draw->pop();
+    }
+}
+
+void ObjectsManager::Add(Object *p)
+{
+    update_list.push_back(p);
+}
+
+void ObjectsManager::DestroyAll()
+{
+    for (auto &&i : update_list)
+    {
+        delete i;
+    }
+}
+
+Game::Game(/* args */) {}
+
+Game::~Game()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    TTF_Quit();
+}
+
+void Game::initEngine(const char *title, int xpos, int ypos, int width,
                       int height, bool windowed)
 {
 
-  h = height;
-  w = width;
+    screen_h = height;
+    screen_w = width;
 
-  gtimer = new timer(0.005);
+    int flags = 0;
 
-  int flags = 0;
-
-  if (windowed)
-  {
-    flags = SDL_WINDOW_FULLSCREEN;
-  }
-
-  if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-  {
-    TTF_Init();
-    cout << "[?] subsystem inited" << endl;
-
-    window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-
-    if (window)
+    if (windowed)
     {
-      cout << "[?] Window created" << endl;
+        flags = SDL_WINDOW_FULLSCREEN;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (renderer)
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-      cout << "[?] Renderer created" << endl;
+        TTF_Init();
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        isrunning = true;
     }
-
-    isrunning = true;
-  }
-  else
-  {
-    isrunning = false;
-  }
-
-  initGame();
+    else
+    {
+        isrunning = false;
+    }
+    initGame();
 }
 
-void game::handleevents()
+void Game::handleevents()
 {
-  SDL_PollEvent(&event);
+    SDL_PollEvent(&event);
 
-  switch (event.type)
-  {
-  case SDL_QUIT:
-    isrunning = false;
-    break;
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        isrunning = false;
+        break;
 
-  default:
-    break;
-  }
+    default:
+        break;
+    }
 }
 
 int i = 0;
 
-void game::update()
+void Game::update()
 {
-  gtimer->FrameTimeout();
-  gtimer->deltaUpdate();
-  updateGame();
+    objecs.Update();
+    updateGame();
 }
 
-void game::render()
+void Game::render()
 {
-  SDL_SetRenderDrawColor(renderer, 20, 20, 30, 0);
-  SDL_RenderClear(renderer);
-  specificRenderBefore();
-  specificRenderAfter();
-  SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 20, 20, 30, 0);
+    SDL_RenderClear(renderer);
+
+    specificRenderBefore();
+    objecs.Draw();
+    specificRenderAfter();
+
+    SDL_RenderPresent(renderer);
 }
 
-void game::clean()
+void Game::clean()
 {
-  SDL_DestroyWindow(window);
-  SDL_DestroyRenderer(renderer);
-  SDL_Quit();
+    objecs.DestroyAll();
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
 }
-
-bool game::running() { return isrunning; }
-
-SDL_Renderer *game::getRenderer() { return renderer; }
-
-SDL_Window *game::getWindow() { return window; }
