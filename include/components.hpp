cc4e89bd8b90d336bf2cc58e4cb177cc89fdabba
liember <liember@yandex.ru>
2020-08-24 23:05:41 +0700

    click area

--- include/components.hpp
+++ include/components.hpp
@@ -4,6 +4,8 @@
 #include "SDL2/SDL_image.h"
 #include "SDL2/SDL_ttf.h"
 
+#include "core.hpp"
+
 #include <string>
 
 /* components is 
@@ -28,9 +30,8 @@ namespace Component
             long _value;
 
         public:
-            long Get() { return _value; }
+            long Get() const { return _value; }
             explicit Integer(long value) { _value = value; }
-            ~Integer();
 
             void Set(int n_val) { _value = n_val; }
             void operator+=(Integer i);
@@ -49,10 +50,9 @@ namespace Component
             double _value;
 
         public:
+            double Get() const { return _value; }
             void Set(double n_val) { _value = n_val; }
-            double Get() { return _value; }
             explicit Floating(double value) { _value = value; }
-            ~Floating();
 
             void operator+=(Floating i);
             void operator+=(long i);
@@ -88,18 +88,44 @@ namespace Component
             T &pos;
 
         public:
-            void Set(T n_val) { _val }
+            void Set(T n_val) { _val = n_val; }
             void Update() override { pos += _val.Get(); }
             Speed(T &pos_data, T val) : pos(pos_data), _val(val) {}
             ~Speed();
         };
-        Speed<Data::Integer>;
-        Speed<Data::Floating>;
 
     } // namespace Updater
 
     namespace Modificator
     {
+        class ClickArea
+        {
+        private:
+            const Component::Data::Floating &x_pos;
+            const Component::Data::Floating &y_pos;
+            const Component::Data::Integer &width;
+            const Component::Data::Integer &height;
+
+            const Core::Coursor &mouse;
+
+            bool clicked;
+            bool holded;
+            bool hovered;
+
+        public:
+            bool isClecked() const;
+            bool isHovered() const;
+            bool isHold() const;
+
+            bool Update();
+
+            ClickArea(const Core::Coursor &cours,
+                      const Component::Data::Floating &x,
+                      const Component::Data::Floating &y,
+                      const Component::Data::Integer &w,
+                      const Component::Data::Integer &h);
+            ~ClickArea();
+        };
 
     } // namespace Modificator
 
